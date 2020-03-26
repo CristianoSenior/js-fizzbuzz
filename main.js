41c37d03188e0c0ce55f26e6697a19f3664de404
Cristiano <valentisenior@gmail.com>
2020-03-26 20:41:29 +0100

    Con stampa grezza su pagina

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
 
 
+
+
+var ricordailprima , bananajoe;
 // Scrivi un programma che stampi i numeri da 1 a 100,
 
 for(var i = 1 ; i <= 100 ; i++) {
@@ -11,20 +14,25 @@ for(var i = 1 ; i <= 100 ; i++) {
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
   if (i%3==0 && i%5==0) {
     console.log("FizzBuzz");
+    bananajoe = "FizzBuzz";
     // console.log(i);
     // e per i multipli di 5 stampi “Buzz”.
   } else if (i%5==0) {
       console.log("Buzz");
+      bananajoe = "Buzz";
       // console.log(i);
   }// Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.
     else if (i%3==0) {
       console.log("Fizz");
+      bananajoe = "Fizz";
   }else {
       console.log(i);
+      bananajoe = i ;
   }
+  ricordailprima = document.getElementById('calcoli').innerHTML;
+  document.getElementById('calcoli').innerHTML = ricordailprima + bananajoe;
 
-
-
+// console.log(Math.floor(Math.random(bananajoe + i)));
 }
 
 
